cb292a5188c3804fb4ff48fe3b95c8a3a76c42a8
Rachel-de-la-torre <64419118+Rachel-de-la-torre@users.noreply.github.com>
2020-06-16 16:58:46 +0200

    Revert "collision functionality"

--- game.js
+++ game.js
@@ -1,39 +1,45 @@
 class Game {
-  constructor() {
-    this.background = new Background();
-    this.obstacles = [];
-    this.width = 1500;
-    this.height = 800;
-  }
-
-  preloadGame() {
-    targets.preload();
-    this.backgroundImgs = [{ src: loadImage("/assets/background0.png") }];
-    this.obstacleImages = [
-      { src: loadImage("/assets/bell.png"), x: 0, height: 100, type: 1 },
-      { src: loadImage("/assets/cherry.png"), x: 0, height: 250, type: 2 },
-      { src: loadImage("/assets/seven.png"), x: 0, height: 400, type: 3 },
-      { src: loadImage("/assets/watermelon.png"), x: 0, height: 550, type: 4 },
-    ];
-  }
-
-  drawingGame() {
-    clear();
-    this.background.drawingBackground();
-    // collision.drawingCollisionImg();
-    if (frameCount % 180 == 0) {
-      let randomElem = this.obstacleImages[
-        Math.floor(Math.random() * this.obstacleImages.length)
-      ];
-      this.obstacles.push(
-        new Obstacles(randomElem.height, randomElem.src, randomElem.type)
-      );
-      console.log(randomElem);
+    constructor(){
+        this.background = new Background();
+        this.obstacles = [];
+        this.width=1500
+        this.height=800
     }
-    this.obstacles.forEach((elem) => {
-      elem.drawingObstacles();
-    });
 
-    targets.drawingTargetsImg();
-  }
+    preloadGame() {
+        targets.preload();
+        this.backgroundImgs = [
+            {src: loadImage("/assets/background0.png")}
+        ];
+        this.obstacleImages = [
+            { src: loadImage("/assets/bell.png"), x: 0, height: 100},
+            { src: loadImage("/assets/cherry.png"), x: 0, height: 250},
+            { src: loadImage("/assets/seven.png"), x: 0, height: 400},
+            { src: loadImage("/assets/watermelon.png"), x: 0, height: 550}
+          ];
+    }
+
+    drawingGame() {
+        clear();
+
+        
+        this.background.drawingBackground();
+        // collision.drawingCollisionImg();
+
+         if(frameCount%180==0){
+            let randomElem=this.obstacleImages[Math.floor(Math.random()*this.obstacleImages.length)];
+            this.obstacles.push(new Obstacles(randomElem.height,randomElem.src));
+            // console.log(randomElem);
+         }
+          this.obstacles.forEach(elem=>{
+             elem.drawingObstacles()
+         }) 
+
+        targets.drawingTargetsImg();
+
+    }
 }
+
+
+
+    
--- main.js
+++ main.js
@@ -3,29 +3,30 @@ const obstacle = new Obstacles();
 const targets = new Targets();
 
 function preload() {
-  game.preloadGame();
+    game.preloadGame();
 }
 
 function setup() {
-  createCanvas(game.width, game.height);
-  obstacle.setup();
+    createCanvas(game.width, game.height);
+   obstacle.setup();
 }
 
 function draw() {
-  game.drawingGame();
+    game.drawingGame();
 }
 
+
 function keyPressed() {
-  game.obstacles.forEach((obs) => {
-    if (keyCode === 189 && obs.collides() && obs.type == 1) {
-      console.log("please work", obs.type);
-      console.log("key press function");
-    } else if (keyCode === 80) {
-      // obstacle.checkCollision.collideCherry();
-    } else if (keyCode === 76) {
-      // obstacle.checkCollision.collideSeven();
-    } else if (keyCode == 188) {
-      // obstacle.checkCollision.collideWatermelon();
+    if (keyCode === 189) {
+        console.log("keyPressed")
     }
-  });
+    else if (keyCode === 80) {
+        // obstacle.checkCollision.collideCherry();
+      }
+    else if (keyCode === 76) {
+        // obstacle.checkCollision.collideSeven();
+    }
+    else if (keyCode == 188) {
+        // obstacle.checkCollision.collideWatermelon();
+    }
 }
